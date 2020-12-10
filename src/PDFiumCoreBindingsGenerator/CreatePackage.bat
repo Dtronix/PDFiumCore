00c2791aee8e77737b74bb3d321075a845b20697
DJGosnell <DJGosnell@gmail.com>
2020-12-10 17:17:24 -0500

    Update to chrominum/4348 Adds FPDFAnnotGetLine(global::PDFiumCore.FpdfAnnotationT annot, global::PDFiumCore.FS_POINTF_ start, global::PDFiumCore.FS_POINTF_ end)

--- src/PDFiumCoreBindingsGenerator/CreatePackage.bat
+++ src/PDFiumCoreBindingsGenerator/CreatePackage.bat
@@ -1,1 +1,1 @@
-PDFiumCoreBindingsGenerator.exe "https://api.github.com/repos/bblanchon/pdfium-binaries/releases/34294127" "0" true >build.log & type build.log
+PDFiumCoreBindingsGenerator.exe "https://api.github.com/repos/bblanchon/pdfium-binaries/releases/34911950" "0" true >build.log & type build.log
