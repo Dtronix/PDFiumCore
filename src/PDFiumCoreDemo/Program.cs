49b1b80bb0c8e160064c36fe1d42c5e9ca41854d
DJGosnell <DJGosnell@gmail.com>
2021-11-01 19:18:49 -0400

    Started work on adding test suite and demo application.

---
+++ src/PDFiumCoreDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PDFiumCore;
+
+namespace PDFiumCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
+            var pages = fpdfview.FPDF_GetPageCount(document);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
