a43f34712608fac10767cfb9a226ce8a157429a0
DJGosnell <DJGosnell@gmail.com>
2021-11-02 11:44:38 -0400

    Added very simple tests to the workflow. Added demo project for rendering a page to a PNG file.

--- src/PDFiumCoreDemo/Program.cs
+++ src/PDFiumCoreDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using PDFiumCore;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
 
 namespace PDFiumCoreDemo
 {
@@ -7,11 +9,69 @@ namespace PDFiumCoreDemo
     {
         static void Main(string[] args)
         {
-            var document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
-            var pages = fpdfview.FPDF_GetPageCount(document);
+            fpdfview.FPDF_InitLibrary();
+            RenderPageToImage();
+        }
 
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+        static void RenderPageToImage()
+        {
+            double pageWidth = 0;
+            double pageHeight = 0;
+            float scale = 1;
+            // White color.
+            uint color = uint.MaxValue;
+            // Load the document.
+            var document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
+            var page = fpdfview.FPDF_LoadPage(document, 0);
+            fpdfview.FPDF_GetPageSizeByIndex(document, 0, ref pageWidth, ref pageHeight);
+
+            var viewport = new Rectangle()
+            {
+                X = 0,
+                Y = 0,
+                Width = pageWidth,
+                Height = pageHeight,
+            };
+            var bitmap = fpdfview.FPDFBitmapCreateEx(
+                    (int)viewport.Width,
+                    (int)viewport.Height,
+                    (int)FPDFBitmapFormat.BGRA,
+                    IntPtr.Zero,
+                    0);
+
+            if (bitmap == null)
+                throw new Exception("failed to create a bitmap object");
+
+            // Leave out if you want to make the background transparent.
+            fpdfview.FPDFBitmapFillRect(bitmap, 0, 0, (int)viewport.Width, (int)viewport.Height, color);
+
+            // |          | a b 0 |
+            // | matrix = | c d 0 |
+            // |          | e f 1 |
+            using var matrix = new FS_MATRIX_();
+            using var clipping = new FS_RECTF_();
+
+            matrix.A = scale;
+            matrix.B = 0;
+            matrix.C = 0;
+            matrix.D = scale;
+            matrix.E = (float)-viewport.X;
+            matrix.F = (float)-viewport.Y;
+
+            clipping.Left = 0;
+            clipping.Right = (float)viewport.Width;
+            clipping.Bottom = 0;
+            clipping.Top = (float)viewport.Height;
+
+            fpdfview.FPDF_RenderPageBitmapWithMatrix(bitmap, page, matrix, clipping, (int)RenderFlags.RenderAnnotations);
+
+
+            var image = new PdfImage(
+                bitmap,
+                (int)pageWidth,
+                (int)pageHeight);
+
+            image.ImageData.SaveAsPng("output.png");
         }
     }
 }
---
+++ src/PDFiumCoreDemo/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFiumCoreDemo
+{
+    struct Rectangle
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+}
