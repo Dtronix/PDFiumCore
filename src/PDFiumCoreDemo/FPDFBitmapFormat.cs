7d7aef97ca46dbf8278014b4e3c6245d92da0d7b
DJGosnell <DJGosnell@users.noreply.github.com>
2021-11-02 22:20:53 -0400

    Added Tests and Demo (#3)
    Added a simple test suite to ensure that the pdfium library is being properly referenced.
    Added demo for how to convert a PDF page into a PNG.

---
+++ src/PDFiumCoreDemo/FPDFBitmapFormat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFiumCoreDemo
+{
+    [Flags]
+    public enum FPDFBitmapFormat
+    {
+        /// <summary>
+        /// Unknown or unsupported format.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Gray scale bitmap, one byte per pixel.
+        /// </summary>
+        Gray = 1,
+
+        /// <summary>
+        /// 3 bytes per pixel, byte order: blue, green, red.
+        /// </summary>
+        BGR = 2,
+
+        /// <summary>
+        /// 4 bytes per pixel, byte order: blue, green, red, unused.
+        /// </summary>
+        BGRx = 3,
+
+        /// <summary>
+        /// 4 bytes per pixel, byte order: blue, green, red, alpha.
+        /// </summary>
+        BGRA = 4
+    }
+}
