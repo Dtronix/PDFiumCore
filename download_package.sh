25b684ba13d721ab35d4d9c8460221e47a975b50
DJGosnell <DJGosnell@users.noreply.github.com>
2021-11-03 21:33:11 -0400

    Build simplification (#4)
    Greatly simplified the building process.
    Binaries are no longer stored inside the repository and are now pulled in via the Github action.
    Versions are now following Chromium version numbers with the build number in the correct version "build" section.
    PDFium version v97.0.4690.0

---
+++ download_package.sh
@@ -0,0 +1,2 @@
+dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net5.0/PDFiumCoreBindingsGenerator.dll 52626723 false
