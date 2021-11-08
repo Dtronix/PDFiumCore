b4a453bb0ba580d3f7a8d30c3ebaf0e743a87e57
DJGosnell <DJGosnell@gmail.com>
2021-11-08 09:16:26 -0500

    PDFium version v98.0.4695.0 chromium/4695 [master] Updated readme.

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net5.0/PDFiumCoreBindingsGenerator.dll 52626723 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net5.0/PDFiumCoreBindingsGenerator.dll 52891427 false
