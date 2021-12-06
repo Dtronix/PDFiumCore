7e8c72cfa5b8fe351b50da8059ca9620bb091808
DJGosnell <DJGosnell@gmail.com>
2021-12-06 13:26:53 -0500

    PDFium version v98.0.4750.0 chromium/4750. Added testing into the CreateBindingsPackage.bat.

--- CreateBindingsPackage.bat
+++ CreateBindingsPackage.bat
@@ -2,4 +2,5 @@ git clean -fxd
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
 dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net5.0/PDFiumCoreBindingsGenerator.dll latest true
 dotnet pack ./src/PDFiumCore/PDFiumCore.csproj -c Release -o ./artifacts/
+dotnet test ./src/PDFiumCore.Tests/PDFiumCore.Tests.csproj
 pause
