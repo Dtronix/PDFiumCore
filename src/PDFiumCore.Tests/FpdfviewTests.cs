500c04c5586538918d0fe382e90392b96fbb6aba
DJGosnell <DJGosnell@gmail.com>
2022-06-27 18:15:51 -0400

    Added a new test for fpdfview.FPDF_LoadMemDocument.

--- src/PDFiumCore.Tests/FpdfviewTests.cs
+++ src/PDFiumCore.Tests/FpdfviewTests.cs
@@ -1,12 +1,11 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 
 namespace PDFiumCore.Tests
 {
     public class FpdfviewTests
     {
-        private FpdfDocumentT _document;
-
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
@@ -22,13 +21,31 @@ namespace PDFiumCore.Tests
         [SetUp]
         public void Setup()
         {
-            _document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
+
         }
 
         [Test]
         public void ReadsPageCount()
         {
-            Assert.AreEqual(1, fpdfview.FPDF_GetPageCount(_document));
+            var document = fpdfview.FPDF_LoadDocument("pdf-sample.pdf", null);
+            Assert.AreEqual(1, fpdfview.FPDF_GetPageCount(document));
+        }
+
+        [Test]
+        public unsafe void FPDF_LoadMemDocument()
+        {
+            using var fs = File.OpenRead("pdf-sample.pdf");
+            var fileBytes = new byte[fs.Length];
+            using var ms = new MemoryStream(fileBytes);
+
+            // Copy file the underlying byte stream.
+            fs.CopyTo(ms);
+
+            fixed (void* ptr = fileBytes)
+            {
+                var document = fpdfview.FPDF_LoadMemDocument(new IntPtr(ptr), fileBytes.Length, null);
+                Assert.AreEqual(1, fpdfview.FPDF_GetPageCount(document));
+            }
         }
     }
 }
