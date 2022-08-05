426328585ff53a0f94f3ea1aaf9bf031edcb0266
DJGosnell <DJGosnell@gmail.com>
2022-08-05 11:56:41 -0400

    Addition of the build_test_publish.ps1 powershell file.

---
+++ build_test_publish.ps1
@@ -0,0 +1,23 @@
+git clean -fxd
+dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll latest true
+dotnet pack ./src/PDFiumCore/PDFiumCore.csproj -c Release -o ./artifacts/
+dotnet test ./src/PDFiumCore.Tests/PDFiumCore.Tests.csproj
+if($?)
+{
+    $xmlNode = Select-Xml -Path src/Directory.Build.props -XPath '/Project/PropertyGroup/Version' 
+    $version = "v" + ($xmlNode.Node.InnerXML)
+	$commitMessage = "PDFium version " + $version
+	git commit -a -m $commitMessage
+    git tag $version
+	
+	$confirmation = Read-Host "Do you want to push the new tag? (y/n)"
+	if ($confirmation -eq 'y') {
+		git push origin $version 
+	}
+}
+else 
+{
+	echo "Tests Failed."
+}
+pause
