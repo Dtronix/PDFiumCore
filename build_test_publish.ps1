c10a5d4768d4b23aba1a18c9fcc513a50b6856fb
DJGosnell <DJGosnell@gmail.com>
2022-08-15 10:02:53 -0400

    Only commits now when the user accepts.

--- build_test_publish.ps1
+++ build_test_publish.ps1
@@ -4,18 +4,23 @@ dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGe
 dotnet pack ./src/PDFiumCore/PDFiumCore.csproj -c Release -o ./artifacts/
 dotnet test ./src/PDFiumCore.Tests/PDFiumCore.Tests.csproj
 if($?)
-{
+{	
     $xmlNode = Select-Xml -Path src/Directory.Build.props -XPath '/Project/PropertyGroup/Version' 
     $version = "v" + ($xmlNode.Node.InnerXML)
 	$commitMessage = "PDFium version " + $version
-	git commit -a -m $commitMessage
-    git tag $version
-	
-	$confirmation = Read-Host "Do you want to push the new tag? (y/n)"
+	$confirmation = Read-Host "Do you want to commit and push the new tag? (y/n)"
 	if ($confirmation -eq 'y') {
-		git push origin
-		git push origin $version 
+	    git commit -a -m $commitMessage
+        git tag $version
+
+	    git push origin
+	    git push origin $version 
+        echo "Pushed tag to origin."
 	}
+    else 
+    {
+        echo "Canceled tag & push."
+    }
 }
 else 
 {
