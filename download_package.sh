75409c5de8d08e72d2eb0fdc1faa9f9f5863987c
DJGosnell <DJGosnell@gmail.com>
2022-10-31 11:52:23 -0400

    PDFium version v109.0.5392.0 +public static int FPDFTextHasUnicodeMapError(global::PDFiumCore.FpdfTextpageT text_page, int index)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 80758254 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 81516808 false
