55dc1aa943445bbb747d41b27c2acf7baff968d8
DJGosnell <DJGosnell@gmail.com>
2023-06-05 10:59:21 -0400

    PDFium version v116.0.5813.0

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 103761796 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 107300577 false
