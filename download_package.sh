e85212dbd8f2b246203f7f02a62c256c3f95504f
DJGosnell <DJGosnell@gmail.com>
2023-08-28 10:35:24 -0400

    PDFium version v118.0.5975.0 chromium/5975 [master] +public static int FPDFTextIsHyphen(global::PDFiumCore.FpdfTextpageT text_page, int index)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 117955665 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 118885448 false
