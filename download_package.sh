19c9e1b0b4b5bb14906652f88ec61338aad41315
DJGosnell <DJGosnell@gmail.com>
2023-10-23 09:53:02 -0400

    PDFium version v120.0.6084.0 chromium/6084 [master] public static int FPDF_StructElementGetChildMarkedContentID(global::PDFiumCore.FpdfStructelementT struct_element, int index)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 124252590 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 126192809 false
