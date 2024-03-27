e565aeaeda670fff10b38967dfba5845bafae02f
djgosnell <djgosnell@gmail.com>
2024-03-27 11:57:51 -0400

    PDFium version v125.0.6378.0 chromium/6378 [master] +public static global::PDFiumCore.FpdfAttachmentT FPDFAnnotGetFileAttachment(global::PDFiumCore.FpdfAnnotationT annot) +public static global::PDFiumCore.FpdfAttachmentT FPDFAnnotAddFileAttachment(global::PDFiumCore.FpdfAnnotationT annot, ref ushort name)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 142657744 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 148145837 false
