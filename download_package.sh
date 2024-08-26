660ffe66b677d65ebe3edd1f5168db42ea6c8380
djgosnell <djgosnell@gmail.com>
2024-08-26 11:23:52 -0400

    PDFium version v130.0.6679.0 chromium/6679 [master] *public static int FPDFBitmapFillRect(global::PDFiumCore.FpdfBitmapT bitmap, int left, int top, int width, int height, uint color) +public static int FPDFCatalogSetLanguage(global::PDFiumCore.FpdfDocumentT document, string language) +public static int FPDFPageObjTransformF(global::PDFiumCore.FpdfPageobjectT page_object, global::PDFiumCore.FS_MATRIX_ matrix) +public static int FPDFPageObjGetMarkedContentID(global::PDFiumCore.FpdfPageobjectT page_object) +public static ulong FPDFFontGetBaseFontName(global::PDFiumCore.FpdfFontT font, sbyte* buffer, ulong length) +public static global::PDFiumCore.FpdfPageobjectT FPDFTextGetTextObject(global::PDFiumCore.FpdfTextpageT text_page, int index) -public static global::PDFiumCore.FPDF_TEXT_RENDERMODE FPDFTextGetTextRenderMode(global::PDFiumCore.FpdfTextpageT text_page, int index)

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 163323669 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll 171884604 false
