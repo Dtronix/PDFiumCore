8fc057be94c24f583f8dd18a1b95c58df22467ff
djgosnell <djgosnell@gmail.com>
2025-01-29 09:57:32 -0500

    Updated build pipeline for dotnet 8 for all projects

--- CreateBindingsPackage.bat
+++ CreateBindingsPackage.bat
@@ -1,6 +1,6 @@
 git clean -fxd
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net6.0/PDFiumCoreBindingsGenerator.dll latest true
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net8.0/PDFiumCoreBindingsGenerator.dll latest true
 dotnet pack ./src/PDFiumCore/PDFiumCore.csproj -c Release -o ./artifacts/
 dotnet test ./src/PDFiumCore.Tests/PDFiumCore.Tests.csproj
 pause
