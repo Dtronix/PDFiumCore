b2871baaad753334cf4b7aa4c1e2aa61955b821c
djgosnell <djgosnell@gmail.com>
2026-02-10 18:26:44 -0500

    Reverted build back to 134.0.6982.0.

--- download_package.sh
+++ download_package.sh
@@ -1,2 +1,2 @@
 dotnet build src/PDFiumCoreBindingsGenerator/PDFiumCoreBindingsGenerator.csproj -c Release
-dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net8.0/PDFiumCoreBindingsGenerator.dll 284405313 false
+dotnet ./src/PDFiumCoreBindingsGenerator/bin/Release/net8.0/PDFiumCoreBindingsGenerator.dll 196869661 false
